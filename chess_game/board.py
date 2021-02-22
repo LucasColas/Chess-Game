51e9bd558e1428dca6f4c6fdce32722207848c67
LucasColas <lucas.colas13@gmail.com>
2021-02-22 07:04:35 +0100

    update

--- chess_game/board.py
+++ chess_game/board.py
@@ -1,6 +1,1 @@
 import pygame
-
-B = "Salut"
-
-def testtttt():
-    print("Test")
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pygame
 
-from chess_game.board import *
 from chess_game.constants import *
 
-Win = pygame.display.set_mode(())
+Win = pygame.display.set_mode((Width, Height))
 
 def update_window(Win):
+    Win.fill(beige)
     pygame.display.update()
 
 def main():
     run = True
 
     while run:
+        update_window(Win)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
+
+main()
