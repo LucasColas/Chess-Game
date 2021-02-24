15077e2d7e0c63c6832146c25e82677eb41de4b0
LucasColas <lucas.colas13@gmail.com>
2021-02-24 07:43:34 +0100

    update

--- chess_game/board.py
+++ chess_game/board.py
@@ -26,7 +26,7 @@ class newBoard:
                 #if (row+1)%2 == 0 and (col+1)%2==0:
                     #pygame.draw.rect(self.Win,brown_chocolate,(Square*(row+1), Square*(col+1),Square,Square))
 
-
+        #Operational
         #for row in range(Rows):
             #for col in range(row%2, Cols,2):
                 #pygame.draw.rect(self.Win,brown_chocolate,(Square*(row), Square*(col),Square,Square))
