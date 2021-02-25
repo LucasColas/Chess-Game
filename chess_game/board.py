dc70609bbb13e7e9071acd678f6810cea68b3978
LucasColas <lucas.colas13@gmail.com>
2021-02-25 07:14:28 +0100

    update

--- chess_game/board.py
+++ chess_game/board.py
@@ -1,7 +1,7 @@
 import pygame
 
 from .constants import Width, Height, Rows,Cols,Square
-from .constants import beige, brown_chocolate
+from .constants import White, brown
 
 
 class newBoard:
@@ -16,17 +16,8 @@ class newBoard:
         #pygame.draw.rect(self.Win, cornsilk, (160, 160,480, 480))
 
     def create_Board(self):
-        self.Win.fill(beige)
+        self.Win.fill(White)
 
         for row in range(Rows):
-            for col in range(Cols):
-                if row%2 == 0 and col%2==0:
-                    pygame.draw.rect(self.Win,brown_chocolate,(Square*row, Square*col,Square,Square))
-
-                #if (row+1)%2 == 0 and (col+1)%2==0:
-                    #pygame.draw.rect(self.Win,brown_chocolate,(Square*(row+1), Square*(col+1),Square,Square))
-
-        #Operational
-        #for row in range(Rows):
-            #for col in range(row%2, Cols,2):
-                #pygame.draw.rect(self.Win,brown_chocolate,(Square*(row), Square*(col),Square,Square))
+            for col in range(row%2, Cols,2):
+                pygame.draw.rect(self.Win,brown,(Square*(row), Square*(col),Square,Square))
