36b9dcb3db5f8fdb7074afdb87215d82b195a6ab
LucasColas <lucas.colas13@gmail.com>
2021-02-28 07:21:28 +0100

    update

--- chess_game/Pieces.py
+++ chess_game/Pieces.py
@@ -1,5 +1,15 @@
 import pygame
 import constants
 
-def test_piece(Win):
-    Win.blit(constants.Black_King, (25,25))
+#def test_piece(Win):
+    #Win.blit(constants.Black_King, (25,25))
+
+
+class Piece:
+    def __init__(self, image):
+        self.image = image
+
+
+class Pawn(Piece):
+    def __init__(self, image):
+        super().__init__(image)
