af9d227380012783677bfb222c76fc772a303937
LucasColas <lucas.colas13@gmail.com>
2021-03-19 07:40:20 +0100

    update

--- chess_game/Pieces.py
+++ chess_game/Pieces.py
@@ -249,15 +249,49 @@ class Knight(Piece):
     def get_available_moves(self,row,col,Board):
         self.clear_available_moves()
 
-        if row-2 >= 0 and col+1 <= len(Board):
-            if Board[row-2][col+1] == 0:
+        if row-2 >= 0 and col+1 < len(Board):
+            if Board[row-2][col+1] == 0 or Board[row-2][col+1].color != self.color:
                 self.available_moves.append((row-2,col+1))
-            else:
-                if Board[row-2][col+1].color != self.color:
-                    self.available_moves.append((row-2,col+1))
-                    break
 
-                break 
+
+        if row-1 >= 0 and col+2 < len(Board):
+            if Board[row-1][col+2] == 0 or Board[row-1][col+2].color != self.color:
+                self.available_moves.append((row-1,col+2))
+
+        if row+1 < len(Board) and col+2 < len(Board):
+            if Board[row+1][col+2] == 0 or Board[row+1][col+2].color != self.color:
+                self.available_moves.append((row+1,col+2))
+
+        if row+2 < len(Board) and col+1 < len(Board):
+            if Board[row+2][col+1] == 0 or Board[row+2][col+1].color != self.color:
+                self.available_moves.append((row+2,col+1))
+
+        if row+2 < len(Board) and col-1 >= 0:
+            if Board[row+2][col-1] == 0 or Board[row+2][col-1].color != self.color:
+                self.available_moves.append((row+2,col-1))
+
+        if row+1 < len(Board) and col-2 >= 0:
+            if Board[row+1][col-2] == 0 or Board[row+1][col-2].color != self.color:
+                self.available_moves.append((row+1,col-2))
+
+
+        if row-1 >= 0 and col-2 > 0:
+            if Board[row-1][col-2] == 0 or Board[row-1][col-2].color != self.color:
+                self.available_moves.append((row-1,col-2))
+
+        if row-2 >= 0 and col-1 > 0:
+            if Board[row-2][col-1] == 0 or Board[row-2][col-1].color != self.color:
+                self.available_moves.append((row-2,col-1))
+
+
+        return self.available_moves
+
+
+
+
+
+
+
 
 class Queen(Piece):
     def __init__(self, Square, image,color,type,row,col):
