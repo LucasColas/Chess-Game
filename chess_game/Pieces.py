3cdf5783711c256b4fa165ce9b0288197668da09
LucasColas <lucas.colas13@gmail.com>
2021-03-20 07:04:47 +0100

    update

--- chess_game/Pieces.py
+++ chess_game/Pieces.py
@@ -293,6 +293,136 @@ class Queen(Piece):
     def get_available_moves(self,row,col,Board):
         self.clear_available_moves()
 
+        row_i = row+1
+        col_i = col+1
+        while row_i <= 7 and col_i <= 7:
+            if Board[row_i][col_i] == 0:
+                #print("first loop : ",row_i, col_i)
+                self.available_moves.append((row_i,col_i))
+                row_i += 1
+                col_i += 1
+
+            else:
+                if Board[row_i][col_i].color != self.color:
+                    #print("first loop : ",row_i,col_i)
+                    self.available_moves.append((row_i,col_i))
+                    break
+                break
+
+        row_i = row-1
+        col_i = col-1
+        while row_i >= 0 and col_i >= 0:
+
+            if Board[row_i][col_i] == 0:
+                self.available_moves.append((row_i,col_i))
+                #print("second loop ",row_i, col_i)
+                row_i -= 1
+                col_i -= 1
+
+            else:
+                if Board[row_i][col_i].color != self.color:
+                    #print("second loop",row_i, col_i)
+                    self.available_moves.append((row_i,col_i))
+                    break
+                break
+
+
+
+        row_i = row
+        col_i = col
+        while row_i >= 0 and col_i < 7:
+            row_i -= 1
+            col_i += 1
+
+            if Board[row_i][col_i] == 0:
+                #print("third loop",row_i, col_i)
+                self.available_moves.append((row_i, col_i))
+
+            else:
+                if Board[row_i][col_i].color != self.color:
+                    #print("third loop",row_i, col_i)
+                    self.available_moves.append((row_i, col_i))
+                    break
+
+                break
+
+        row_i = row
+        col_i = col
+        while row_i < 7 and col_i > 0:
+            row_i += 1
+            col_i -= 1
+            print(Board[row_i][col_i])
+            if Board[row_i][col_i] == 0:
+                #print("fourth loop",row_i, col_i)
+                self.available_moves.append((row_i, col_i))
+
+            else:
+                if Board[row_i][col_i].color != self.color :
+                    #print("fourth loop",row_i, col_i)
+                    self.available_moves.append((row_i, col_i))
+                    break
+
+                break
+
+
+        for i in range(row+1, 8):
+
+            if Board[i][col] == 0:
+
+                self.available_moves.append((i,col))
+            else:
+                if Board[i][col].color != self.color:
+
+                    self.available_moves.append((i,col))
+                    break
+                else:
+                    break
+
+        for j in range(row-1,-1,-1):
+
+            if Board[j][col] == 0:
+
+                self.available_moves.append((j,col))
+
+            else:
+                if Board[j][col].color != self.color:
+
+                    self.available_moves.append((j,col))
+                    break
+
+                else:
+
+                    break
+
+        for i in range(col+1, 8):
+            if Board[row][i] == 0:
+                self.available_moves.append((row,i))
+
+            else:
+                if Board[row][i].color != self.color:
+                    self.available_moves.append((row,i))
+                    break
+
+                else:
+                    break
+
+        for i in range(col-1, -1,-1):
+
+            if Board[row][i] == 0:
+                self.available_moves.append((row,i))
+
+            else:
+                if Board[row][i].color != self.color:
+                    self.available_moves.append((row,i))
+                    break
+
+                else:
+                    break
+                    
+        return self.available_moves
+
+
+
 class King(Piece):
     def __init__(self,Square, image,color,type,row,col):
         super().__init__(Square, image,color,type,row,col)
